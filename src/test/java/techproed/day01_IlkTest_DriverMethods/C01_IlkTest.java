086ba9dc47e86d44e410f33b8ecf90b93748b762
Tuba icer <ecrentuba@gmail.com>
2023-05-26 22:34:37 +0200

    Initial commit

---
+++ src/test/java/techproed/day01_IlkTest_DriverMethods/C01_IlkTest.java
@@ -0,0 +1,24 @@
+package techproed.day01_IlkTest_DriverMethods;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class C01_IlkTest {
+
+    public static void main(String[] args) {
+        System.setProperty("chromeDriver","src/resources/drivers/chromedriver.exe");
+        //Java uygulamalarinda system ozelliklerini ayarlamak icin setProperty methodu ile kullaniriz.
+        //setProperty methodu ile class`ìmiza driver`in fiziki yolunu belirtiriz.
+
+        System.out.println(System.getProperty("chromeDriver"));
+        //getProperty ile "Key" degerini girerek "value" ya ulasabilirim.
+
+        WebDriver driver = new ChromeDriver();
+        //ChromeDriver türünde yeni bir obje oluşturduk
+
+        driver.get("https://www.techproeducation.com");
+        //get() methodu ile String olarak girilen url'e gideriz
+
+
+        }
+}
