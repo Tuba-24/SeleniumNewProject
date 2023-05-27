f1a1f879b1c44a3f61928e42e9e8fb15dbb8513c
Tuba icer <ecrentuba@gmail.com>
2023-05-27 22:29:25 +0200

    27/05/23

---
+++ src/test/java/techproed/day02_DriverMethods/C02_NavigateMethods.java
@@ -0,0 +1,43 @@
+package techproed.day02_DriverMethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C02_NavigateMethods {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        //Techproeducation sayfasina gidelim
+        driver.navigate().to("https://techproeducation.com");//get() methodu ile ayni mantikla calisir
+        Thread.sleep(3000);//Java kodlarini bekletmek icin Thread.sleep kullanabiliriz
+
+        //Sonra Amazon sayfasina gidelim
+        driver.navigate().to("https://amazon.com");
+        Thread.sleep(3000);
+
+        //Amazon sayfasinin basligini yazdiralim
+        System.out.println("Amazon Sayfa Basligi : " +driver.getTitle());
+        Thread.sleep(3000);
+
+        //Techproeducation sayfasina geri donelim
+        driver.navigate().back();
+        Thread.sleep(3000);
+
+        //Sayfa basligini yazdiralim
+        System.out.println("Techproeducation Sayfa Basligi : "+driver.getTitle());
+        Thread.sleep(3000);
+
+        //Amazon sayfasina tekrar gidip url`i yazdiralim
+        driver.navigate().forward();
+        System.out.println("Amazon Sayfasi Url`i : "+driver.getCurrentUrl());
+        Thread.sleep(3000);
+
+        //Amazon sayfasindayken sayfayi yenileyelim
+        driver.navigate().refresh();
+        Thread.sleep(3000);
+
+        //Sayfayi kapatalim
+        driver.close();
+    }
+}
